17ade84016c220c0db5ad538d476b8075a73e185
jceron <jelsync@gmail.com>
2025-12-03 11:08:57 -0600

    Practica 2, uso de random_uuid para la creacion de  bucket

--- practica_2/bucket.tf
+++ practica_2/bucket.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
-provider "aws" {
-  region  = "us-east-2"      # ajustar según donde quieras crear recursos
-  profile = "personal"
-}
-
 resource "aws_s3_bucket" "proveedores" {
-  bucket = "proveedores-jelsyn-test-bucket"
+  bucket = "bucket-${random_uuid.test.result}"
 }
