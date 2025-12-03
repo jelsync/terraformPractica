59b7d90d9d09979b45e913283737953a03d159c7
jceron <jelsync@gmail.com>
2025-12-03 11:15:23 -0600

    Tags en bucket

--- practica_2/bucket.tf
+++ practica_2/bucket.tf
@@ -1,3 +1,8 @@
 resource "aws_s3_bucket" "proveedores" {
   bucket = "bucket-${random_uuid.test.result}"
+   tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+    owner       = "Jelsyn"
+  }
 }
