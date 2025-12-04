c9bb2b99bce94e926893c97cc1d13d45a6f2487a
jceron <jelsync@gmail.com>
2025-12-04 15:29:58 -0600

    Refactorizacion subnet

--- practica_5/vpc.tf
+++ practica_5/vpc.tf
@@ -4,20 +4,3 @@ resource "aws_vpc" "vpc_ohio" {
     "Name" = "VPC_OHIO"
   }
 }
-
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.vpc_ohio.id
-  cidr_block              = var.subnet[0]
-  map_public_ip_on_launch = true
-  tags = {
-    "Name" = "public_subnet"
-  }
-}
-
-resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.vpc_ohio.id
-  cidr_block = var.subnet[1]
-  tags = {
-    "Name" = "private_subnet"
-  }
-}
