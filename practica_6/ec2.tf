8c8f12d5ac13bed9280f601689409aedbabc215d
jceron <jelsync@gmail.com>
2025-12-04 16:11:52 -0600

    Uso de data caso de uso key par, para recursos ya utilizados o creados en la nube

--- practica_6/ec2.tf
+++ practica_6/ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "public_instance" {
   ami           = "ami-025ca978d4c1d9825"
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.public_subnet.id
+  key_name      = data.aws_key_pair.key.key_name
 
   tags = {
     Name = "HelloWorld"
